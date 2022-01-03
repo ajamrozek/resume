8173865c4273fa2e205a6fe091189892fe3d5902
ajamrozek <ajamrozek@msn.com>
2022-01-02 20:14:29 -0700

    better page breaks for printing

--- resume-app/src/App.js
+++ resume-app/src/App.js
@@ -1,38 +1,29 @@
 import './App.css';
 import React, { Component } from 'react';
+import ReactDOM from "react-dom";
 import { format } from 'date-fns'
-// import * as AWS from 'aws-sdk'
-// import { ConfigurationOptions } from 'aws-sdk'
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       resumeData: null
     };
 
-    // const awsConfiguration = {
-    //   region:'us-east-2',
-    //   secretAccessKey: '7Zn9Z/qlj7olR5E5//LU/vpYfgHvoLBFV5kO/T70',
-    //   accessKeyId: 'AKIA332UJJCOVQY3TBGD'
-    // }
-  
-    // AWS.config.update(awsConfiguration);
-
     this.SkillTemplate = this.SkillTemplate.bind(this);
     this.SkillsTemplate = this.SkillsTemplate.bind(this);
   }
 
-  SkillTemplate = function(props) {
+  SkillTemplate = function (props) {
     // if(!props.skills){
     //   return null;
     // }
     return props.skills.map(skill => <li key={skill}>{skill}</li>);
   };
 
-  SkillsTemplate = function(props) {
-    if(!props.skills){
+  SkillsTemplate = function (props) {
+    if (!props.skills) {
       return null;
     }
     var skillKeys = Object.keys(props.skills);
@@ -42,14 +33,14 @@ class App extends Component {
         <self.SkillTemplate key={skillKey + "Skill"} skills={props.skills[skillKey]} />
       </ul></div>)
     return <div className="allSkills">{skillTemplate}</div>;
-  
+
   };
-  
-  EducationTemplate = function(props) {
-    if(!props.education){
+
+  EducationTemplate = function (props) {
+    if (!props.education) {
       return null;
     }
-    
+
     var educationTemplate = props.education
       .sort((a, b) => a.start < b.start ? 1 : -1)
       .map((edu, idx) => <div key={"edu" + idx} className={"summary education"}>
@@ -62,18 +53,41 @@ class App extends Component {
           <span className="field">{edu.field}</span>
         </div>
       </div>);
-  
+
     return <div className={"history allEducation"}>{educationTemplate}</div>;
   };
-  
-  JobsTemplate = function(props) {
-    if(!props.jobs){
+
+
+  JobsTemplate = function (props) {
+    if (!props.jobs) {
       return null;
     }
-    
+
+    var lastBreak = -100;
+    var applyPageBreak = function (target) {
+      const pageThreshold = 809;
+
+      if (target.children.length > 0) {
+        for (let childIdx = 0; childIdx < target.children.length; childIdx++) {
+          const childElement = target.children[childIdx];
+
+          applyPageBreak(childElement);
+        }
+      }
+      else {
+        var workingTop = target.offsetTop;
+        target.setAttribute("telemetry", JSON.stringify({ workingTop }));
+
+        if (workingTop - lastBreak >= pageThreshold) {
+          lastBreak = workingTop;
+          target.classList.add('pg-break');
+        }
+      }
+    };
+
     var jobsTemplate = props.jobs
       .sort((a, b) => Date.parse(a.start) < Date.parse(b.start) ? 1 : -1)
-      .map((job, idx) => <div key={"job" + idx} className={"summary job"}>
+      .map((job, idx) => <div id={"job" + idx} key={"job" + idx} className={"summary job"}>
         <span className="name">{job.clientName}</span>
         <span className="right">
           <span className="location">{job.location}</span>
@@ -82,45 +96,42 @@ class App extends Component {
         <div className="title-terms">
           <span className="title">{job.title.trim()} - {job.terms}</span>
         </div>
-        <div className="details" dangerouslySetInnerHTML={{ __html: job.details.join("") }} />
-  
+        <div className="details"
+          ref={el => {
+            // el can be null - see https://reactjs.org/docs/refs-and-the-dom.html#caveats-with-callback-refs
+            if (!el) return;
+
+            applyPageBreak(el);
+          }}
+          dangerouslySetInnerHTML={{ __html: job.details.join("") }} />
+
       </div>)
-  
+
     return <div className={"history allJobs"}>{jobsTemplate}</div>;
   }
 
-  GetResumeData = function(){
-    // const docClient = new AWS.DynamoDB.DocumentClient();
-    // var params = {
-    //   TableName: "resume"
-    // };
-    //let self = this;
-    // docClient.scan(params, function (err, data) {
-    //   if (!err) {
-    //     self.setState({resumeData: data.Items[0]});
-    //   }
-    // });
-    this.setState({resumeData: require('./resume.json')});
+  GetResumeData = function () {
+    this.setState({ resumeData: require('./resume.json') });
   };
-  
 
   render() {
     return (
-    <div className="App">
-      
-      <header className="App-header">
-        <h1 style={{ marginBottom: 0 }}>Alex Jamrozek</h1>
-        <h3 style={{ margin: 0 }}>{this.state.resumeData?.title}</h3>
-        <p >{this.state.resumeData?.tagLine} </p>
-        <this.SkillsTemplate skills={this.state.resumeData?.skills} />
-        <this.EducationTemplate education={this.state.resumeData?.education} />
-      </header>
-      <this.JobsTemplate jobs={this.state.resumeData?.jobs} />
-    </div>);
+      <div className="App">
+
+        <header className="App-header">
+          <h1 style={{ marginBottom: 0 }}>Alex Jamrozek</h1>
+          <h3 style={{ margin: 0 }}>{this.state.resumeData?.title}</h3>
+          <p >{this.state.resumeData?.tagLine} </p>
+          <this.SkillsTemplate skills={this.state.resumeData?.skills} />
+          <this.EducationTemplate education={this.state.resumeData?.education} />
+        </header>
+        <this.JobsTemplate jobs={this.state.resumeData?.jobs} />
+      </div>);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.GetResumeData();
+
   }
 }
 
